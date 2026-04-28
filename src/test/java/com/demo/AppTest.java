27c74dca447e2c4426f5f3bca6f154ed82dfb274
Ubuntu <ubuntu@ip-172-31-8-169.ap-south-1.compute.internal>
2026-04-28 11:07:17 +0000

    fixed pom.xml structure

---
+++ src/test/java/com/demo/AppTest.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
